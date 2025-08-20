f17e7406bf2758a9692c97033721b04a62dee08b
Tech-Ninja478 <dcadityapachpute@gmail.com>
2025-08-20 19:28:17 +0530

    Services file created and the Services Data also changed

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -1,33 +1,33 @@
 const Services_Data = [
     {
         s_no:"01",
-        s_name:"Web design",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Web Development",
+        s_desc:"Full-Stack Web Developer with expertise in building responsive and dynamic websites. Proficient in HTML, CSS, JavaScript, and various frameworks.",
     },
     {
         s_no:"02",
-        s_name:"Graphics design",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Mobile App Development",
+        s_desc:"Experienced in developing mobile applications for Android platforms. Skilled in Flutter for cross-platform development.",
     },
     {
         s_no:"03",
-        s_name:"Social media",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"IoT Development",
+        s_desc:"Expert in IoT solutions, specializing in smart home automation and device integration. Proficient in using Raspberry Pi and Arduino for IoT projects.",
     },
     {
         s_no:"04",
-        s_name:"App design",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Machine Learning with LLMs",
+        s_desc:"Currently learning Machine Learning and Large Language Models, with hands-on experience through short projects.",
     },
     {
         s_no:"05",
-        s_name:"Digital marketing",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Data Mining",
+        s_desc:"Skilled in data mining techniques, including web scraping and data analysis. Proficient in Python libraries and striving to learn more.",
     },
     {
         s_no:"06",
-        s_name:"Content writing",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Marketing",
+        s_desc:"Served as the marketing admin for my college committee and helped secure multiple sponsorships for the annual Tech-Fest.",
     },
 ]
  
