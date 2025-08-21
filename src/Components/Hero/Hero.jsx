6727be6b0cb6ccd777b4801797a3ef244ac417ea
Tech-Ninja478 <dcadityapachpute@gmail.com>
2025-08-21 20:47:03 +0530

    Smooth Scroll Implemented

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ import profile_img from '../../assets/profile_img.svg'
 
 const Hero = () => {
     return (
-        <div className='hero'>
+        <div id='home' className='hero'>
             <img src={profile_img} alt="" />
             <h1><span>I'm Aditya,</span> B-Tech Information Technology</h1>
             <p>Passionate about web development and modern JavaScript frameworks.
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,21 +1,56 @@
-import React from 'react'
-import './Navbar.css'
-import logo from '../../assets/logo.png'
+import React, { useState } from 'react';
+import './Navbar.css';
+import logo from '../../assets/logo.png';
+import underline from '../../assets/nav_underline.svg';
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const Navbar = () => {
-  return (
-    <div className='navbar'>
-        <img src={logo} alt="" width={250} height={85} />
-        <ul className="nav-menu">
-            <li>Home</li>
-            <li>About Me</li>
-            <li>Services</li>
-            <li>Portfolio</li>
-            <li>Contact</li>
-        </ul>
-        <div className="nav-connect">Connect With Me</div>
-    </div>
-  )
-}
+  const [menu, setMenu] = useState("home");
 
-export default Navbar
+  return (
+    <div className="navbar">
+      <img src={logo} alt="logo" width={250} height={85} />
+
+      <ul className="nav-menu">
+        <li>
+          <AnchorLink className="anchor-link" offset={50} href="#home">
+            <p onClick={() => setMenu("home")}>Home</p>
+          </AnchorLink>
+          {menu === "home" && <img src={underline} alt="underline" />}
+        </li>
+
+        <li>
+          <AnchorLink className="anchor-link" offset={50} href="#about">
+            <p onClick={() => setMenu("about")}>About Me</p>
+          </AnchorLink>
+          {menu === "about" && <img src={underline} alt="underline" />}
+        </li>
+
+        <li>
+          <AnchorLink className="anchor-link" offset={50} href="#services">
+            <p onClick={() => setMenu("services")}>Services</p>
+          </AnchorLink>
+          {menu === "services" && <img src={underline} alt="underline" />}
+        </li>
+
+        <li>
+          <AnchorLink className="anchor-link" offset={50} href="#work">
+            <p onClick={() => setMenu("work")}>Portfolio</p>
+          </AnchorLink>
+          {menu === "work" && <img src={underline} alt="underline" />}
+        </li>
+
+        <li>
+          <AnchorLink className="anchor-link" offset={50} href="#contact">
+            <p onClick={() => setMenu("contact")}>Contact</p>
+          </AnchorLink>
+          {menu === "contact" && <img src={underline} alt="underline" />}
+        </li>
+      </ul>
+
+      <div className="nav-connect">Connect With Me</div>
+    </div>
+  );
+};
+
+export default Navbar;
