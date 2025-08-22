801863576ec70e377d5e4af5a185420b76da7bf3
Tech-Ninja478 <dcadityapachpute@gmail.com>
2025-08-22 17:06:30 +0530

    Changed Images and Styles them

--- src/Components/MyWork/MyWork.jsx
+++ src/Components/MyWork/MyWork.jsx
@@ -5,23 +5,36 @@ import mywork_data from '../../assets/mywork_data'
 import arrow_icon from '../../assets/arrow_icon.svg'
 
 const MyWork = () => {
-  return (
-    <div id='work' className='mywork'>
-        <div className="mywork-title">
-            <h1>My Latest Works</h1>
-            <img src={theme_pattern} alt="" />
+    const handleShowMore = (githubLink) => {
+        window.open(githubLink, '_blank');
+    };
+
+    return (
+        <div id='work' className='mywork'>
+            <div className="mywork-title">
+                <h1>My Latest Works</h1>
+                <img src={theme_pattern} alt="" />
+            </div>
+            <div className="mywork-container">
+                {mywork_data.map((work, index) => (
+                    <div className="mywork-item" key={index}>
+                        <img src={work.w_img} alt={work.title} className="mywork-img" />
+                        <h2 className="mywork-project-title">{work.w_name}</h2>
+                        <button
+                            className="mywork-showmore-btn"
+                            onClick={() => handleShowMore(work.githubLink)}
+                        >
+                            Show More
+                        </button>
+                    </div>
+                ))}
+            </div>
+            <div className="mywork-showmore">
+                <p>Show More</p>
+                <img src={arrow_icon} alt="" />
+            </div>
         </div>
-        <div className="mywork-container">
-            {mywork_data.map((work, index) => {
-                return <img key={index} src={work.w_img} alt="" />
-            })}
-        </div>
-        <div className="mywork-showmore">
-            <p>Show More</p>
-            <img src={arrow_icon} alt="" />
-        </div>
-    </div>
-  )
+    )
 }
 
 export default MyWork
